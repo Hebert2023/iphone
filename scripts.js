1dacddd8ece90f59f96172ee8c8c8d40ee9dcad6
hebert2023 <hebertalvescarvalho@gmail.com>
2023-03-31 20:18:57 -0300

    atualização do codigo

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+function trocarcor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background=cor
+}
+function trocarimagem(imagem){
+    let imgIphone = document.querySelector(".Iphone")
+    imgIphone.src=imagem
+}
